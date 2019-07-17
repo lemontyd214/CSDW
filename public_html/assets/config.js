b95a3ed32331dd05e3a739fa05f94d462057d9c3
lemontyd214 <teng.y@husky.neu.edu>
2019-07-17 11:54:24 +0200

    first time backup website

---
+++ public_html/assets/config.js
@@ -0,0 +1,33 @@
+// cpanel - site_templates/countdown_tech/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "",
+        logo: "CSDW",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            endTime: ''
+        }
+    ]
+};
